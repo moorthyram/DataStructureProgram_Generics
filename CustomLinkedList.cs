e7d98d2a1b3bfe0899e621c51557ce969731cbca
moorthyram <91622436+moorthyram@users.noreply.github.com>
2022-08-08 07:06:29 +0530

    Update CustomLinkedList.cs

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -10,7 +10,7 @@ namespace DataStructureProgram_Generics
     {
 
             public Node head;
-            public void Add(int data)
+            public void Addlast(int data)
             {
                 Node node = new Node(data);
                 if (head == null)
